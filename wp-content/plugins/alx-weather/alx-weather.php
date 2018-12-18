0784556fb807231c4830da4c65d169bad6675d6e
GolovaVasilenko <vasilenko75@gmail.com>
2018-12-18 13:39:01 +0200

    start creation plugin

---
+++ wp-content/plugins/alx-weather/alx-weather.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+Plugin Name: ALX Weather
+Plugin URI: http://lanars.com
+Description: Отображение погоды на странице сайта.
+Version: 1.0
+Author: Aleksej Vasilenko
+Author URI: http://lanars.com
+*/
+
+$api_key = '6d42fe5e407536a60abd6077b2095c55';
+
