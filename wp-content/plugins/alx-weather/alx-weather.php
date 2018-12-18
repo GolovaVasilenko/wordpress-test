d9c0019d0c0a8bae69d6590aa7af87b70df958e1
GolovaVasilenko <vasilenko75@gmail.com>
2018-12-18 15:26:10 +0200

    created function connection api weather

--- wp-content/plugins/alx-weather/alx-weather.php
+++ wp-content/plugins/alx-weather/alx-weather.php
@@ -9,5 +9,56 @@ Author: Aleksej Vasilenko
 Author URI: http://lanars.com
 */
 
-$api_key = '6d42fe5e407536a60abd6077b2095c55';
+define("PLUGIN_PATH", __DIR__);
 
+class ALX_Weather
+{
+    protected $api_key = '&APPID=6d42fe5e407536a60abd6077b2095c55';
+
+    protected $url     = 'http://api.openweathermap.org/data/2.5/weather';
+
+    /**
+     * @param string $template
+     * @return mixed
+     */
+    public function getMapTemplate($template = 'TA2')
+    {
+        return $this->type_maps[$template];
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->api_key;
+    }
+
+    public static function test()
+    {
+        return PLUGIN_PATH;
+    }
+
+    public static function getDataApi()
+    {
+        $weather = new self;
+
+        $city = '?q=San Francisco';
+        $country_cod = 'us';
+
+        $url  = $weather->getUrl() . $city . $country_cod . $weather->api_key;
+
+        $content = file_get_contents($url);
+
+        var_dump($content);
+    }
+
+}
